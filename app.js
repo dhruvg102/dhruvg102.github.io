ce148ddc7e4b094b66e105ccd981e3f1585f3f93
dhruvg102 <dhruv.gupta1500@gmail.com>
2021-05-06 23:37:00 +0100

    Testing 2

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import { OrbitControls } from 'C:/Users/dhruv/OneDrive/Documents/GitHub/dhruvg102.github.io/libs/three/jsm/OrbitControls.js';
 
 class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
-    
+
         window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
+	}
+
     resize(){
-        
+
     }
-    
-	render( ) {   
-        
+
+	render( ) {
+
     }
 }
 
