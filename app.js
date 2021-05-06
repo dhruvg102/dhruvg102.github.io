15e760aa17eb5c0b3a68fc0fb0aa7f6cc08f0b07
dhruvg102 <dhruv.gupta1500@gmail.com>
2021-05-06 23:49:25 +0100

    testing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.119.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.119.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './libs/three/three.module.jss';
+import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
 
 class App{
 	constructor(){
