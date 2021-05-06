a07360e8be461d4af9b0b27f97f1d444b230f125
dhruvg102 <dhruv.gupta1500@gmail.com>
2021-05-07 00:19:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class App{
 		this.camera.position.set( 0, 0, 4 );
 
 		this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0x777777 );
+        this.scene.background = new THREE.Color( 0x333333 );
 
 		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
 		this.scene.add(ambient);
